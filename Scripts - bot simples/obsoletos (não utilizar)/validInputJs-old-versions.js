792c30ca1217a8399fff44eb3655f2d42e3b9a5f
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2022-09-27 18:23:22 -0300

    2022-09-27 validInputJs
    Correção no novo validInputJs para tratar inputs que não devem ser validados, mantendo as duas constantes validacaoMenu e validacaoInput como false

---
+++ Scripts - bot simples/obsoletos (não utilizar)/validInputJs-old-versions.js
@@ -0,0 +1,45 @@
+// ================================================================================
+// OLD VERSION: MARCIA
+// ================================================================================
+// function run(input, menu) {
+//     try {
+//         menu = JSON.parse(menu);
+//         input = input.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+//         input = input.split(" ")[0]
+//         const opcao = menu.itens;
+
+//         for (i = 0; i < opcao.length; i++) {
+//             for (x = 0; x < opcao[i].name.length; x++) {
+//                 if (opcao[i].name[x].includes(input)) {
+//                     return opcao[i].name[0]
+//                 }
+//             }
+//         }
+//         return 'INPUT INESPERADO'
+//     }
+//     catch (e) {
+//         return 'error'
+//     }
+// }
+// ================================================================================
+// OLD VERSION: BRUNO
+// ================================================================================
+// function run(input, menu) {
+//     try {
+//         menu = JSON.parse(menu);
+//         const opcao = menu.itens;
+
+//         for (i = 0; i < opcao.length; i++) {
+//             for (x = 0; x < opcao[i].name.length; x++) {
+//                 if (opcao[i].name[x] == input) {
+//                     return opcao[i].name[0]
+//                 }
+//             }
+//         }
+//         return 'INPUT INESPERADO'
+//     }
+//     catch (e) {
+//         return 'validInputJS ERROR: UNEXPECTED ERROR'
+//     }
+// }
+// ================================================================================
