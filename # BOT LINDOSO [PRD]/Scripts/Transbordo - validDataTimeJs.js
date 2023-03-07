a98fa2c9b883bc494da4f57f8e7a78f8d810b4d6
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2023-03-06 23:54:35 -0300

    Bot Lindoso Update 2023-03-07

--- # BOT LINDOSO [PRD]/Scripts/Transbordo - validDataTimeJs.js
+++ # BOT LINDOSO [PRD]/Scripts/Transbordo - validDataTimeJs.js
@@ -1,51 +1,51 @@
-let holiday = JSON.stringify(["1-1","2-21","4-2","4-21","5-1","9-7","10-8","10-12","11-2","11-15", "12-25","12-31"])
-let workTime = JSON.stringify({
-  "weekdays": [{
-      "day": "Domingo",
-      "id": 0,
-      "hourStart": "",
-      "hourEnd": ""
-  },
-  {
-      "day": "Segunda",
-      "id": 1,
-      "hourStart": "0900",
-      "hourEnd": "1800"
-  },
-  {
-      "day": "Terça",
-      "id": 2,
-      "hourStart": "0900",
-      "hourEnd": "1800"
-  },
-  {
-      "day": "Quarta",
-      "id": 3,
-      "hourStart": "0900",
-      "hourEnd": "1800"
-  },
-  {
-      "day": "Quinta",
-      "id": 4,
-      "hourStart": "0900",
-      "hourEnd": "1800"
-  },
-  {
-      "day": "Sexta",
-      "id": 5,
-      "hourStart": "0900",
-      "hourEnd": "1800"
-  },
-  {
-      "day": "Sábado",
-      "id": 6,
-      "hourStart": "",
-      "hourEnd": ""
-  }
-  ]
-})
+// let holiday = JSON.stringify(["1-1","2-21","4-2","4-21","5-1","9-7","10-8","10-12","11-2","11-15", "12-25","12-31"])
+// let workTime = JSON.stringify({
+//   "weekdays": [{
+//       "day": "Domingo",
+//       "id": 0,
+//       "hourStart": "",
+//       "hourEnd": ""
+//   },
+//   {
+//       "day": "Segunda",
+//       "id": 1,
+//       "hourStart": "0900",
+//       "hourEnd": "1800"
+//   },
+//   {
+//       "day": "Terça",
+//       "id": 2,
+//       "hourStart": "0900",
+//       "hourEnd": "1800"
+//   },
+//   {
+//       "day": "Quarta",
+//       "id": 3,
+//       "hourStart": "0900",
+//       "hourEnd": "1800"
+//   },
+//   {
+//       "day": "Quinta",
+//       "id": 4,
+//       "hourStart": "0900",
+//       "hourEnd": "1800"
+//   },
+//   {
+//       "day": "Sexta",
+//       "id": 5,
+//       "hourStart": "0900",
+//       "hourEnd": "1800"
+//   },
+//   {
+//       "day": "Sábado",
+//       "id": 6,
+//       "hourStart": "",
+//       "hourEnd": ""
+//   }
+//   ]
+// })
 
-console.log(run(holiday, workTime))
+// console.log(run(holiday, workTime))
 
 function run(holiday, workTime) {
   try {
