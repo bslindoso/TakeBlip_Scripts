72c35b7fa69f626bc52352432ba46c38dc03b324
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2023-04-13 23:50:33 -0300

    2023-04-13 Nova versão dos scripts

---
+++ # Bot Lindoso [TEMPLATE]/Scripts/BOT BOAS VINDAS - [REQUIREMENTS] - wanumber.js
@@ -0,0 +1,26 @@
+function run(contact) {
+    
+    // Sem o código do país         
+    // let phone = contact.split("@")[0].replace("55", "");   
+
+    // Com o código do país
+    let phone = contact.split("@")[0];   
+    let tamanho = phone.length;
+
+
+    if(tamanho == 10){
+        
+        let telefone = phone.split('').join().replace(/\D/g, '');
+        let math = Math.floor(telefone.length - 8 ); 
+        let concat = telefone.substr(0,math)+"9"+telefone.substr(math);
+        let retorno = concat;
+
+        return retorno;
+    }
+    if(tamanho == 11){
+        
+        let retorno = phone
+        return retorno;
+    }
+
+}
